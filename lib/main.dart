5ae75d093f79eeaf127c53ac023c83cb75fdca3a
OceanT-shirt <stylish.trousers@gmail.com>
2022-03-17 10:49:13 +0900

    created button

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Noah',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,13 +22,17 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.indigo,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "NOAH"),
     );
   }
 }
 
+
+
+// widgetクラスとstateクラスのセットで表される
+// widgetクラス
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -47,8 +51,12 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+// stateクラス
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  var _title = "potato";
+  final List<String> entries = <String>['A', 'B', 'C'];
+  final List<int> colorCodes = <int>[600, 500, 100];
 
   void _incrementCounter() {
     setState(() {
@@ -57,7 +65,21 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      if (_counter >= 10) {
+        _counter = 0;
+      } else {
+        _counter++;
+      }
+    });
+  }
+
+  void _potatoSalad() {
+    setState(() {
+      if (_title == "potato") {
+        _title = "potato salad";
+      } else {
+        _title = "potato";
+      }
     });
   }
 
@@ -75,10 +97,11 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: Align(
+        alignment: Alignment.topLeft,
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
+        child: ListView(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
           // children horizontally, and tries to be as tall as its parent.
@@ -93,23 +116,84 @@ class _MyHomePageState extends State<MyHomePage> {
           // center the children vertically; the main axis here is the vertical
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+
+            Text(
+              'In Progress',
+              style: Theme.of(context).textTheme.headline4,
             ),
             Text(
-              '$_counter',
+              _title,
+            ),
+            Text(
+              "Review",
               style: Theme.of(context).textTheme.headline4,
+            ),
+            Card(
+              child: InkWell(
+                splashColor: Colors.blue.withAlpha(30),
+                onTap: (){
+                  debugPrint("Hoge");
+                },
+                child: SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Column(
+                      children: <Widget>[
+                        Row(children: [
+                          Container(
+                            margin: const EdgeInsets.all(20),
+                            child: const Icon(
+                              Icons.favorite,
+                            ),
+                          ),
+                          Column(
+                            children: [
+                            Text(
+                                "ProgramNameSample",
+                              style: Theme.of(context).textTheme.headline6,
+                              textAlign: TextAlign.center,
+                            ),
+                            Text("40%",
+                            style: Theme.of(context).textTheme.subtitle1,),
+                            ],
+                          ),
+                        ],),
+                        const LinearProgressIndicator(
+                          value: 0.4,
+                          color: Colors.red,
+                          minHeight: 20,
+                        ),
+                      ]
+                  ),
+                ),
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _potatoSalad,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label:  'Learn'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.doorbell),
+              label: 'Emergency'
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Account',
+          ),
+        ],
+        selectedItemColor: Colors.red[900],
+      ),
     );
   }
 }
